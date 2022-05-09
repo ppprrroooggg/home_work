a29a9bf575dfb7fd55e5218df96804330beb68ef
DariaE15-1 <74792345+DariaE15-1@users.noreply.github.com>
2022-05-09 17:51:51 +0300

    Initial commit

---
+++ main_reg.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.register import RegisterForm
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data)
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init("db/mars_explorer.db")
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
